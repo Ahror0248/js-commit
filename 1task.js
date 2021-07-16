9ddac10d35566ebd93b2fe6d641d142fa6158aec
Ahror0248 <akhrorcodrr@gmail.com>
2021-07-16 11:49:43 +0500

    done

---
+++ 1task.js
@@ -0,0 +1,12 @@
+dog = {
+    name: 'Rex',
+    legs: 4,
+    color: 'black',
+    age: 3,
+    bark: 'woofwoof'
+}
+
+console.log( Object.keys(dog))
+dog.breed = 'meat'
+dog.getDogInfo = 'healthy'
+console.log(dog);
---
+++ Level2.js
@@ -0,0 +1,81 @@
+const users = {
+  Alex: {
+    email: 'alex@alex.com',
+    skills: ['HTML', 'CSS', 'JavaScript'],
+    age: 20,
+    isLoggedIn: false,
+    points: 30
+  },
+  Asab: {
+    email: 'asab@asab.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
+    age: 25,
+    isLoggedIn: false,
+    points: 50
+  },
+  Brook: {
+    email: 'daniel@daniel.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+    age: 30,
+    isLoggedIn: true,
+    points: 50
+  },
+  Daniel: {
+    email: 'daniel@alex.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+    age: 20,
+    isLoggedIn: false,
+    points: 100
+  },
+  John: {
+    email: 'john@john.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+    age: 20,
+    isLoggedIn: true,
+    points: 500
+  },
+  Thomas: {
+    email: 'thomas@thomas.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+    age: 20,
+    isLoggedIn: false,
+    points: 40
+  },
+  Paul: {
+    email: 'paul@paul.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
+    age: 20,
+    isLoggedIn: false,
+    points: 40
+  }
+}
+
+const USERS = Object.entries(users)
+
+console.log(USERS);
+
+// for(let i=0; i<USERS.length; i++) {
+//     if(points >= 50){
+//         return 'points: more than 50'
+//     }
+// }
+
+
+let arr=[];
+let h = USERS.map(item=>{
+
+    item.map(obj=>{
+        if(typeof obj === 'object'){
+            obj.skills.map(itm=>{
+                 if(itm === 'MongoDB' && itm==='Express' && itm === 'React' && itm==='Node'){
+                    arr.push(obj)
+                 }})  
+    }
+})
+
+
+
+})
+
+
+// console.log(arr);
